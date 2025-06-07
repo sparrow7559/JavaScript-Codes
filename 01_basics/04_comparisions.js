941d6746b26c5703a9c0b50d1718cc5fc93a3efb
sparrow7559 <virajgupta2604@gmail.com>
2025-06-08 00:59:09 +0530

    Comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log("\n");
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log("\n");
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// Null and undefined comparisions are very confusing
+
+//===
+console.log("\n");
+
+console.log("2"===2);
+

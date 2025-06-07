7c35a3c51974fce2adfb0198f51f619c8d9231c5
sparrow7559 <virajgupta2604@gmail.com>
2025-06-08 00:33:31 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valInNum = Number(score);
+console.log(typeof (valInNum));
+console.log(valInNum);
+
+// "33"+.33
+//"33abc"=> NaN
+// true=>1; false=>0;
+
+let isloggedIn ="Viraj";
+let booleanisloggedIn = Boolean(isloggedIn);
+console.log(booleanisloggedIn);
+
+// 1=>true; 0=>false
+//"Viraj"=> true
+
+let someNum = 33;
+let stringNum= String(someNum);
+console.log( typeof stringNum);

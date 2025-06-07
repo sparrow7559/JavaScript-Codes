f31494df9dc506496ed57cc84db8059e66899364
sparrow7559 <virajgupta2604@gmail.com>
2025-06-08 00:05:20 +0530

    First Commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accID = 14512;
+let accEmail ="virajgupta@gmail.com";
+var accPass = "12345";
+accCity="Bhopal";
+
+accEmail="vda@gmail.com"
+accPass="2641651";
+accCity="Mumbai";
+
+// accID=2;
+console.log(accID);
+console.table([accID,accEmail,accPass,accCity]);
---
+++ first.js
@@ -0,0 +1,19 @@
+name="Viraj rahul gupta";
+console.log(name);
+radius =14.00;
+console.log("Circumference: "+2*3.14*radius);
+// alert(name);
+let age=34;
+// alert(age);
+const fname="vi";
+console.log(fname);
+price = 100.5;
+isfollow=true;
+console.log(isfollow);
+const student ={
+    fullname: "Viraj Rahul Gupta",
+    id:101,
+    ispass:true,
+};
+student.id+=1;
+console.log("Hello");

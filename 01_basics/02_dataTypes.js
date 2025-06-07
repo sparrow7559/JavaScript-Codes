24b8464f52ea181fa52462d1984279cde9534ad7
sparrow7559 <virajgupta2604@gmail.com>
2025-06-08 00:21:56 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS cose as newer version
+
+// alert(3+3);
+// we are using node js not browser
+
+let name = "Viraj";
+let age= 18;
+let isPass=true;
+
+//number => 2^53
+//bigint
+// string=""
+// boolean => true/false
+// null => standalone value 
+// undefined => no value assinged 
+// symbol => unique
+
+//object
+
+console.log(typeof "Viraj");
+console.log(typeof age);
+console.log(typeof null);

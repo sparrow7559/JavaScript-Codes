f3c62ed2873c8b2d9ad04317ed9329743d3521ca
sparrow7559 <virajgupta2604@gmail.com>
2025-06-13 00:56:52 +0530

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,7 +28,36 @@ function loginUserMsg(username="Sam"){
     }
     return `${username} just logged in`;
 }
- console.log(loginUserMsg("Viraj"));
- console.log(loginUserMsg());
+//  console.log(loginUserMsg("Viraj"));
+//  console.log(loginUserMsg());
 
+ function calcartprice(val1,val2,...num1){
+        return num1;
+ }
+
+//  console.log(calcartprice(200,400,500));
  
+const user={
+    username:"Viraj",
+    price:199
+};
+
+function handleobject(anyobj){
+    console.log(`Username is ${anyobj.username} and the price is ${anyobj.price}`);
+    
+}
+
+// handleobject(user); 
+handleobject({
+    username:'sam',
+    price:399
+}); 
+
+const myNewArray=[200,400,100,600];
+
+function returnSecVal(getArray){
+    return getArray[1];
+}
+
+// console.log(returnSecVal(myNewArray));
+console.log(returnSecVal([1,2,3,4]));

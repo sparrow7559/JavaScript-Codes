acd69cc491944bb545d488ecf610b8bfb6e6f18d
sparrow7559 <virajgupta2604@gmail.com>
2025-06-13 01:05:06 +0530

    Scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+// var c=300;//global scope
+let a =300;
+if (true){
+    let a =10;
+    const b=20;
+    console.log("Inner: ",a);
+    
+}//block scope
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+    
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);

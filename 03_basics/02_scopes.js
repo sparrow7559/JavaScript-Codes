17e0865f2454572149443be1943ef65961a6e70e
sparrow7559 <virajgupta2604@gmail.com>
2025-06-13 01:55:05 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,17 +3,58 @@ let a =300;
 if (true){
     let a =10;
     const b=20;
-    console.log("Inner: ",a);
+    // console.log("Inner: ",a);
     
 }//block scope
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+ 
+
+function one(){
+    const username ="Viraj";
+
+    function two(){
+        const website="Youtube";
+        console.log(username);
+        
+    }
+    // console.log(website);
+    // two();
     
+}
+// one();
+
+if(true){
+    const username="viraj";
+    if(username==="viraj"){
+        const website="youtube";
+        // console.log(username+website);
+        
+    }
+    // console.log(website);//error
     
+}
+// console.log(username);//error
+
+//++++++++++++++++++Interesting++++++++++++++
+
+console.log(addone(5));
+
+
+function addone(num){
+    return num+1;
+
 }
 
 
-console.log(a);
-// console.log(b);
-// console.log(c);
+addtwo(5);
+
+const addtwo=function(num){
+    return num+2;
+}
+
